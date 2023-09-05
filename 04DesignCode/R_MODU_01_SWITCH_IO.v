18d22c5d85a35b2842c71f8737dfeb5387992a26
RJwwJR <rjw_wjr@outlook.com>
2023-09-05 23:04:10 +0800

    对于Switch IO 进行设计
    根据实际写代码的需要，对于system和 Switch IO 模块的 IO 声明都进行了更新

--- 04DesignCode/R_MODU_01_SWITCH_IO.v
+++ 04DesignCode/R_MODU_01_SWITCH_IO.v
@@ -21,6 +21,23 @@
 
 
 module SWITCH_IO(
+  input CLK,
+  input [9:0] SW,
+  input [9:0] SW_History_In,
 
-    );
+  output [4:0] Up_Queue,
+  output [4:0] Down_Queue,
+  output [9:0] SW_History_Out,
+  output [15:0] Code,
+  output [2:0] Code_Bit);
+  integer i;
+  
+  ////R:当发生改变，立即处理信号，由于 SWITCH 电平改变未必和时钟一致，因此应该为组合逻辑
+  always@ (SW)
+  begin
+    for ( i = 0; i <= 9 ; i = i + 1)
+    begin
+    end
+  end
+
 endmodule
