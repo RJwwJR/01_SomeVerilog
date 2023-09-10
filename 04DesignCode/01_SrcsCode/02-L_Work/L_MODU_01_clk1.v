9cdcc58f12268e30185fa02fa895f5492207ffc0
RJwwJR <rjw_wjr@outlook.com>
2023-09-10 23:48:54 +0800

    从远程库更新协作的代码
    1.状态的具体情况
    2.分频、消抖等模块更新
    3.显示模块是否衔接？

---
+++ 04DesignCode/01_SrcsCode/02-L_Work/L_MODU_01_clk1.v
@@ -0,0 +1,56 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/09/04 13:24:59
+// Design Name: 
+// Module Name: L_CLK1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module L_MOUDU_CLK1MS(CLK,RST_N,CLK1MS);
+input CLK;
+input RST_N;
+output CLK1;
+reg CLK1;
+reg [19:0] count;
+always @(posedge clk or negedge RST_N)
+begin
+  if(!RST_N)
+    begin
+      CLK1 <= 1'b0;
+      count <= 0;
+    end
+  else if(count == 50000)
+    begin
+      CLK1 <= ~CLK1;
+      count <= count + 1;
+    end
+    else if(count == 100000)
+      begin
+        CLK1 <= ~CLK1;
+        count <= 0;
+      end
+    else
+      begin
+        CLK1 <= CLK1;
+        count <= count + 1;
+        end
+     
+end
+
+endmodule
+
+
